8c31144778d1ba1483278d3b1c954565827538dd
Marco Malvicini <marco.malvicini31@gmail.com>
2020-12-07 11:38:58 +0100

    Starting files create-react-app plus tic-tac-toe edited by Marco.

---
+++ src/components/game.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import Board from "./board";
+import calculateWinner from "../utilities/calculateWinner";
+
+function Game(props) {
+  const [gameStatus, setGameStatus] = useState({
+    history: [
+      {
+        squares: Array(9).fill(null),
+      },
+    ],
+    stepNumber: 0,
+    xIsNext: true,
+  });
+
+  function handleClick(i) {
+    const history = gameStatus.history.slice(0, gameStatus.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = gameStatus.xIsNext ? "X" : "O";
+    setGameStatus({
+      history: [
+        ...history,
+        {
+          squares: squares,
+        },
+      ],
+      stepNumber: history.length,
+      xIsNext: !gameStatus.xIsNext,
+    });
+  }
+
+  function jumpTo(step) {
+    setGameStatus((prevValue) => {
+      return { ...prevValue, stepNumber: step, xIsNext: step % 2 === 0 };
+    });
+  }
+
+  const history = gameStatus.history;
+  const current = history[gameStatus.stepNumber];
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (gameStatus.xIsNext ? "X" : "O");
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button
+          onClick={() => {
+            jumpTo(move);
+          }}
+        >
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
